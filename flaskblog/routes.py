b46e91cfb0a620cc5a0494450155e8fa8e889017
tanknavy <cheng_alex@126.com>
2020-08-26 15:45:21 -0700

    web paginate

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,10 +1,10 @@
 import os
 import secrets
 from PIL import Image
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.models import User, Post
-from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm # 自定义form,现在从包中的模块中引用
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm # 自定义form,现在从包中的模块中引用
 from flask_login import login_user, current_user, logout_user, login_required
 #-----------上述引用全部放到项目同名的包下面的__init__.py文件中
 
@@ -44,7 +44,16 @@ def home():
     # return "<h3>Hello Mr. Millions Cheng</h3>" #返回html语法
     # 使用网页模板渲染，网页在同级的templates目录下
     # Flask使用Jinjia2作为template engine
-    return render_template('home.html', posts=blog_list)
+    # 全部查询
+    #posts = Post.query.all()
+    # 使用分页
+    # ?page=2可选参数查看第二页
+    page = request.args.get('page', 1, type=int) #自定义第几页输出，默认1
+    # 全部查询，按照时间栏位倒序排列
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2) #post是一个paginate对象了，html也要更新
+    #   {% for post in posts.items %}
+    # 使用?page=2
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
@@ -148,3 +157,76 @@ def account():
 
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='account', image_file=image_file, form=form)  # 使用网页模板渲染
+
+
+@app.route("/post/new", methods=['GET','POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been create!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Post')  # 使用网页模板渲染
+
+
+# 单帖子显示
+@app.route("/post/<int:post_id>") #注意<>中不能有空格
+def post(post_id):
+    post = Post.query.get_or_404(post_id) # 存在或者返回404
+    return render_template('post.html', title='Post', post=post)  # 使用网页模板渲染
+
+
+@app.route("/post/<int:post_id>/update", methods=['GET','POST']) #注意<>中不能有空格
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id) #数据model
+    #只能修改自己的帖子
+    if post.author != current_user:
+        abort(403)
+    form = PostForm() #view视图
+    # 更新已有的帖子
+    if form.validate_on_submit(): # POST
+        post.title = form.title.data
+        post.content = form.content.data
+        #db.session.add(post) #这里无需创建post，因为post已经存在并且读出
+        db.session.commit() #提交更新
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id)) #返回更新后的帖子
+    elif request.method == 'GET': #展示
+        #修改前默认先展示要修改的帖子
+        form.title.data = post.title #读取现有数据
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', post=post, form=form, legend='Update Post')  # 使用网页模板渲染
+
+
+@app.route("/post/<int:post_id>/delete", methods=['POST']) #提交删除请求
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)  # 数据model
+    # 只能修改自己的帖子，如果不是自己的帖子网页中按钮也会隐藏
+    if post.author != current_user:
+        abort(403) # 权限不足
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route("/")
+@app.route("/user/<string:username>")  # 让这两个路由指向同一网页
+def user_posts(username):
+    #posts = Post.query.all()
+    # 使用分页
+    # ?page=2可选参数查看第二页
+    page = request.args.get('page', 1, type=int) #自定义第几页输出，默认1
+    user = User.query.filter_by(username=username).first_or_404()
+    # 全部查询，按照时间栏位倒序排列
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=2) #post是一个paginate对象了，html也要更新
+    #   {% for post in posts.items %}
+    # 使用?page=2
+    return render_template('user_posts.html', posts=posts, user=user)
